e2f83165b14fcb852cf5502b3f64178b3b6d3ed3
Bonnie Schulkin <biz@bonnieschulkin.com>
2022-06-29 15:15:10 -0700

    Update configuration file and syntax for Cypress 10

--- tested-concert-venue-nextjs12.2-cypress10.3/cypress.config.ts
+++ tested-concert-venue-nextjs12.2-cypress10.3/cypress.config.ts
@@ -1,11 +1,24 @@
-import { defineConfig } from 'cypress'
+import { resetDB } from "__tests__/__mocks__/db/utils/reset-db";
+import { defineConfig } from "cypress";
+import { addBand } from "lib/features/bands/queries";
+import { addReservation } from "lib/features/reservations/queries";
 
 export default defineConfig({
+  env: {
+    REVALIDATION_SECRET: process.env.REVALIDATION_SECRET,
+    // to access within a test function:
+    //  Cypress.env("REVALIDATION_SECRET")
+  },
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      on("task", {
+        "db:reset": () => resetDB().then(() => null),
+        addBand: (newBand) => addBand(newBand).then(() => null),
+        addReservation: (newReservation) =>
+          addReservation(newReservation).then(() => null),
+      });
     },
   },
-})
+});
