f93589f20c2c1f2505f9b20cf62c4a3fb799bf71
Bonnie Schulkin <biz@bonnieschulkin.com>
2023-01-19 11:58:58 -0800

    Add .nvmrc files

---
+++ tested-concert-venue/cypress/integration/isr/bands-isr.test.ts
@@ -0,0 +1,15 @@
+it("skips client-side bundle, confirming data from ISR cache", () => {
+  // reference: https://glebbahmutov.com/blog/ssr-e2e/#removing-application-bundle
+  cy.request("/bands")
+    .its("body")
+    .then((html: string) => {
+      // remove the scripts, so they don't start automatically
+      const staticHtml = html.replace(/<script.*?>.*?<\/script>/gm, "");
+      cy.state("document").write(staticHtml);
+    });
+
+  // find expected bands
+  cy.findByRole("heading", { name: /the wandering bunnies/i }).should("exist");
+  cy.findByRole("heading", { name: /shamrock pete/i }).should("exist");
+  cy.findByRole("heading", { name: /the joyous nun riot/i }).should("exist");
+});
